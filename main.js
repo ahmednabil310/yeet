3a9000c66cfd98511128c85681b84341a68c57b7
El-bana <ibrahimaelbana@gmail.com>
2021-05-12 13:14:31 +0200

    navbar scrollspy

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var tokenmocis = {
-  distance: '30%',
-  origin: 'bottom',
+  distance: "30%",
+  origin: "bottom",
   opacity: 0,
 };
 var text = {
@@ -10,12 +10,12 @@ var text = {
 var title = {
   opacity: 0,
   delay: 250,
-  distance: '150%',
+  distance: "150%",
 };
-ScrollReveal().reveal('.tokenmocis-data-inner', tokenmocis);
-ScrollReveal().reveal('#tokenmocies-first', { delay: 200 });
-ScrollReveal().reveal('#tokenmocies-second', { delay: 300 });
-ScrollReveal().reveal('#tokenmocies-third', { delay: 350 });
-ScrollReveal().reveal('.timeline-section__header__p', text);
-ScrollReveal().reveal('.timeline-section__header__buttons', text);
-ScrollReveal().reveal('.timeline-section__header__h1', title);
+ScrollReveal().reveal(".tokenmocis-data-inner", tokenmocis);
+ScrollReveal().reveal("#tokenmocies-first", { delay: 200 });
+ScrollReveal().reveal("#tokenmocies-second", { delay: 300 });
+ScrollReveal().reveal("#tokenmocies-third", { delay: 350 });
+ScrollReveal().reveal(".timeline-section__header__p", text);
+ScrollReveal().reveal(".timeline-section__header__buttons", text);
+ScrollReveal().reveal(".timeline-section__header__h1", title);
