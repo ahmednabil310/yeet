a2af2583c0806e49ab871fc2d11ba3000f926c3b
ahmed <an30458@gmail.com>
2021-05-12 12:54:02 +0200

    finishing animation

---
+++ main.js
@@ -0,0 +1,21 @@
+var tokenmocis = {
+  distance: '30%',
+  origin: 'bottom',
+  opacity: 0,
+};
+var text = {
+  opacity: 0,
+  delay: 150,
+};
+var title = {
+  opacity: 0,
+  delay: 250,
+  distance: '150%',
+};
+ScrollReveal().reveal('.tokenmocis-data-inner', tokenmocis);
+ScrollReveal().reveal('#tokenmocies-first', { delay: 200 });
+ScrollReveal().reveal('#tokenmocies-second', { delay: 300 });
+ScrollReveal().reveal('#tokenmocies-third', { delay: 350 });
+ScrollReveal().reveal('.timeline-section__header__p', text);
+ScrollReveal().reveal('.timeline-section__header__buttons', text);
+ScrollReveal().reveal('.timeline-section__header__h1', title);
